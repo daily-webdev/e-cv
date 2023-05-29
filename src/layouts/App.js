57d9061f1e01c655e957faca15fc16501184ba27
daily-webdev <taublermarcin@gmail.com>
2023-05-29 22:44:21 +0200

    add fonts

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,6 +1,5 @@
 import React, { PureComponent, useState, Suspense, lazy } from "react";
 import { BrowserRouter, Route, NavLink, Routes } from "react-router-dom";
-import "../styles/app.scss";
 // const Lazy = lazy(() => import("./Lazy"));
 import Header from "./Header";
 import Main from "./Main";
