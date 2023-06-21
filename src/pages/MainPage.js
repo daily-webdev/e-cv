ce7856d08ca22076c870dc630d15a15dd1989287
daily-webdev <taublermarcin@gmail.com>
2023-06-21 22:27:41 +0200

    add main page cloud animation

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,10 +1,18 @@
 import React, { PureComponent, useState } from "react";
 import "../styles/pages.scss";
+import "../styles/mainpage.scss"
 
 function MainPage() {
   return (
     <>
-      <div className="page page__main">main page</div>
+      <section className="page page__main">
+        <div className="display">
+          <img className="cloud1" src="../../public/img/cloud.png" alt="" />
+          <img className="cloud2" src="../../public/img/cloud.png" alt="" />
+          <img className="cloud3" src="../../public/img/cloud.png" alt="" />
+
+          </div>
+      </section>
     </>
   );
 }
