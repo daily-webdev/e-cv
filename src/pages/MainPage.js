6f1fc7eac79c53684c97d1ab4b6c3f7d5cb36f3d
daily-webdev <taublermarcin@gmail.com>
2023-06-24 20:23:23 +0200

    add main page text animation

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -19,20 +19,20 @@ function MainPage() {
     <>
       <section className="page page__main">
         <div className="display">
-          <img className="cloud1" src="../../public/img/cloud.png" alt="" />
+          {/* <img className="cloud1" src="../../public/img/cloud.png" alt="" />
           <img className="cloud2" src="../../public/img/cloud.png" alt="" />
-          <img className="cloud3" src="../../public/img/cloud.png" alt="" />
-          <div className="image threeFirstDelay"><img  src="../../public/img/me1.jpg" alt="" /></div>
-          <div className="image threeSecondDelay">   <img  src="../../public/img/me2.jpg" alt="" /></div>
-          <div className="image threeThirdDelay"><img  src="../../public/img/me3.jpg" alt="" /></div>
+          <img className="cloud3" src="../../public/img/cloud.png" alt="" /> */}
+          <div className="image threeFirstMove"><img  src="../../public/img/me1.jpg" alt="" /></div>
+          <div className="image threeSecondMove">   <img  src="../../public/img/me2.jpg" alt="" /></div>
+          <div className="image threeThirdMove"><img  src="../../public/img/me3.jpg" alt="" /></div>
           <div className="about">
             <ul className="list">
-              <li><span><FaHouseUser /></span>Kraków</li>
-              <li><span><FaBirthdayCake /></span>7 września 1996</li>
-              <li><span><MdNaturePeople /></span>przyrodnik</li>
-              <li><span><MdOutlineSportsGymnastics /></span>sportowiec</li>
-              <li><span><MdComputer /></span>informatyk</li>
-              <li><span><FaMusic /></span>muzykant</li>
+              <li className="about1"><span><FaHouseUser /></span>Kraków</li>
+              <li className="about2"><span><FaBirthdayCake /></span>7 września 1996</li>
+              <li className="about3"><span><MdNaturePeople /></span>przyrodnik</li>
+              <li className="about4"><span><MdOutlineSportsGymnastics /></span>sportowiec</li>
+              <li className="about5"><span><MdComputer /></span>informatyk</li>
+              <li className="about6"><span><FaMusic /></span>muzykant</li>
             </ul>
           </div>
           
