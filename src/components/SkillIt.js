5f9540f7cc73a3cd9dab44bf18295f0fdf41a9a2
daily-webdev <taublermarcin@gmail.com>
2023-08-19 22:58:45 +0200

    add media for 1900 px

--- src/components/SkillIt.js
+++ src/components/SkillIt.js
@@ -2,27 +2,45 @@ import React, { PureComponent, useState } from "react";
 import "../styles/components.scss";
 
 function SkillIt() {
-    return (  
-        <>
-        <ul className="list">
-<li>tworzenie aplikacji frontend + podstawowy backend w React + NodeJS, Express, MongoDB</li>
-<li>znajomość React, NodeJS, JS, Express, MongoDB, GIT, BEM, CSS, SASS, HTML</li>
-<li>obsługa VSC, Mongo Atlas/Compass, Github, Heroku</li>
-<li>obsługa MS Office, Corel, Proxmox, Active Directory, Comarch ERP</li>
-<li>biegła obsługa komputera + Windows</li>
-<li>rozwiązywanie problemów informatycznych, sieciowych, sprzętowych</li>
-<li>manualne testowanie oprogramowania, przygotowywanie stanowisk pracy</li>
-<li>przygotowywanie materiałów marketingowych, reklamowych, copywriting, znajdowanie błędów w dokumentacji</li>
-<li></li>
-<li></li>
-<li></li>
-<li></li>
-        </ul>
-        <div className="image twoFirst"><img src="../../public/img/it1.jpg" alt="" /></div>
-        <div className="image twoSecond"><img src="../../public/img/it2.jpg" alt="" /></div>
-        
-        </>
-    );
+  return (
+    <>
+      <ul className="list">
+        <li>
+          tworzenie aplikacji frontend + podstawowy backend w React + NodeJS,
+          Express
+        </li>
+        <li>
+          znajomość React, NodeJS, JS, Express, MongoDB, GIT, BEM, CSS, SASS,
+          HTML
+        </li>
+        <li>obsługa VSC, Mongo Atlas/Compass, Github, Vercel</li>
+        <li>
+          obsługa MS Office, Corel, Proxmox, Active Directory, Comarch ERP
+        </li>
+        <li>biegła obsługa komputera + Windows</li>
+        <li>
+          rozwiązywanie problemów informatycznych, sieciowych, sprzętowych
+        </li>
+        <li>
+          manualne testowanie oprogramowania, przygotowywanie stanowisk pracy
+        </li>
+        <li>
+          przygotowywanie materiałów marketingowych, reklamowych, copywriting,
+          znajdowanie błędów w dokumentacji
+        </li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <div className="image twoFirst">
+        <img src="../../public/img/it1.jpg" alt="" />
+      </div>
+      <div className="image twoSecond">
+        <img src="../../public/img/it2.jpg" alt="" />
+      </div>
+    </>
+  );
 }
 
 export default SkillIt;
--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,5 +1,11 @@
 import React, { PureComponent, useState, Suspense } from "react";
-import { BrowserRouter, Route, NavLink, Routes, useLocation } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  NavLink,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import "../styles/main.scss";
 import MainPage from "../pages/MainPage";
 import Skills from "../pages/Skills";
@@ -12,39 +18,80 @@ import Quotes from "../pages/Quotes";
 import Contact from "../pages/Contact";
 
 function Main() {
-
   const location = useLocation();
   const currentPath = location.pathname;
   let backgroundStyle;
 
-switch (true) {
-  case currentPath.startsWith("/skills") : backgroundStyle = {"background-image": "url('http://localhost:8080/public/img/sand7.jpg')"};
-  break;
-  case currentPath.startsWith("/exp") : backgroundStyle = {"background-image": "url('http://localhost:8080/public/img/sand9.jpg')"};
-  break;
-  case currentPath.startsWith("/blogs") : backgroundStyle = {"background-image": "url('http://localhost:8080/public/img/sand3.jpg')"};
-  break;
-  case currentPath.startsWith("/edu") : backgroundStyle = {"background-image": "url('http://localhost:8080/public/img/sand5.jpg')"};
-  break;
-  case currentPath.startsWith("/hobby") : backgroundStyle = {"background-image": "url('http://localhost:8080/public/img/sand12.jpg')"};
-  break;
-  case currentPath.startsWith("/vol") : backgroundStyle = {"background-image": "url('http://localhost:8080/public/img/sand11.jpg')"};
-  break;
-  case currentPath.startsWith("/quotes") : backgroundStyle = {"background-image": "url('http://localhost:8080/public/img/sand8.jpg')"};
-  break;
-  case currentPath.startsWith("/skills") : backgroundStyle = {"background-image": "url('http://localhost:8080/public/img/sand10.jpg')"};
-  break;
-  default: backgroundStyle = {"background-image": "url('http://localhost:8080/public/img/sand4.jpg')"};
-  break
-}
+  switch (true) {
+    case currentPath.startsWith("/skills"):
+      backgroundStyle = {
+        "background-image":
+          "url('http://cv.web-develop.pl/public/img/sand7.jpg')",
+      };
+      break;
+    case currentPath.startsWith("/exp"):
+      backgroundStyle = {
+        "background-image":
+          "url('http://cv.web-develop.pl/public/img/sand4.jpg')",
+      };
+      break;
+    case currentPath.startsWith("/blogs"):
+      backgroundStyle = {
+        "background-image":
+          "url('http://cv.web-develop.pl/public/img/sand3.jpg')",
+      };
+      break;
+    case currentPath.startsWith("/edu"):
+      backgroundStyle = {
+        "background-image":
+          "url('http://cv.web-develop.pl/public/img/sand5.jpg')",
+      };
+      break;
+    case currentPath.startsWith("/hobby"):
+      backgroundStyle = {
+        "background-image":
+          "url('http://cv.web-develop.pl/public/img/sand12.jpg')",
+      };
+      break;
+    case currentPath.startsWith("/vol"):
+      backgroundStyle = {
+        "background-image":
+          "url('http://cv.web-develop.pl/public/img/sand11.jpg')",
+      };
+      break;
+    case currentPath.startsWith("/quotes"):
+      backgroundStyle = {
+        "background-image":
+          "url('http://cv.web-develop.pl/public/img/sand8.jpg')",
+      };
+      break;
+    case currentPath.startsWith("/skills"):
+      backgroundStyle = {
+        "background-image":
+          "url('http://localhost:8080/public/img/sand10.jpg')",
+      };
+      break;
+    default:
+      backgroundStyle = {
+        "background-image":
+          "url('http://cv.web-develop.pl/public/img/sand9.jpg')",
+      };
+      break;
+  }
 
   return (
     <>
-    <div className="background" style={backgroundStyle}></div>
+      <div className="background" style={backgroundStyle}></div>
       <section className="main">
-        
         <Routes>
-          <Route path="/" element={<Suspense fallback={null}><MainPage /></Suspense>}></Route>
+          <Route
+            path="/"
+            element={
+              <Suspense fallback={null}>
+                <MainPage />
+              </Suspense>
+            }
+          ></Route>
           <Route path="/skills/*" element={<Skills />}></Route>
           <Route path="/exp/*" element={<Exp />}></Route>
           <Route path="/blogs" element={<Blogs />}></Route>
@@ -54,51 +101,125 @@ switch (true) {
           <Route path="/quotes" element={<Quotes />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
         </Routes>
-        
       </section>
       <section className="aside">
         <nav className="aside__menu">
           <ul>
             <li>
-              <NavLink className="link" to="/skills/it" style={currentPath.startsWith("/skills") ? {"box-shadow": "0px 0px 10px 5px rgba(111, 79, 28, 0.737)"} : {}}>
+              <NavLink
+                className="link"
+                to="/skills/it"
+                style={
+                  currentPath.startsWith("/skills")
+                    ? {
+                        "box-shadow":
+                          "0px 0px 10px 5px rgba(111, 79, 28, 0.737)",
+                      }
+                    : {}
+                }
+              >
                 umiejętności
               </NavLink>
             </li>
             <li>
-              <NavLink className="link" to="/exp/arvex" style={currentPath.startsWith("/exp") ? {"box-shadow": "0px 0px 10px 5px rgba(111, 79, 28, 0.737)"} : {}}>
+              <NavLink
+                className="link"
+                to="/exp/arvex"
+                style={
+                  currentPath.startsWith("/exp")
+                    ? {
+                        "box-shadow":
+                          "0px 0px 10px 5px rgba(111, 79, 28, 0.737)",
+                      }
+                    : {}
+                }
+              >
                 doświadczenie
               </NavLink>
             </li>
             <li>
-              <NavLink className="link" to="/blogs" style={currentPath.startsWith("/blogs") ? {"box-shadow": "0px 0px 10px 5px rgba(111, 79, 28, 0.737)"} : {}}>
+              <NavLink
+                className="link"
+                to="/blogs"
+                style={
+                  currentPath.startsWith("/blogs")
+                    ? {
+                        "box-shadow":
+                          "0px 0px 10px 5px rgba(111, 79, 28, 0.737)",
+                      }
+                    : {}
+                }
+              >
                 projekty
               </NavLink>
             </li>
             <li>
-              <NavLink className="link" to="/edu" style={currentPath.startsWith("/edu") ? {"box-shadow": "0px 0px 10px 5px rgba(111, 79, 28, 0.737)"} : {}}>
+              <NavLink
+                className="link"
+                to="/edu"
+                style={
+                  currentPath.startsWith("/edu")
+                    ? {
+                        "box-shadow":
+                          "0px 0px 10px 5px rgba(111, 79, 28, 0.737)",
+                      }
+                    : {}
+                }
+              >
                 wykształcenie
               </NavLink>
             </li>
             <li>
-              <NavLink className="link" to="/hobby/sports" style={currentPath.startsWith("/hobby") ? {"box-shadow": "0px 0px 10px 5px rgba(111, 79, 28, 0.737)"} : {}}>
+              <NavLink
+                className="link"
+                to="/hobby/sports"
+                style={
+                  currentPath.startsWith("/hobby")
+                    ? {
+                        "box-shadow":
+                          "0px 0px 10px 5px rgba(111, 79, 28, 0.737)",
+                      }
+                    : {}
+                }
+              >
                 zainteresowania
               </NavLink>
             </li>
             <li>
-              <NavLink className="link" to="/vol" style={currentPath.startsWith("/vol") ? {"box-shadow": "0px 0px 10px 5px rgba(111, 79, 28, 0.737)"} : {}}>
+              <NavLink
+                className="link"
+                to="/vol"
+                style={
+                  currentPath.startsWith("/vol")
+                    ? {
+                        "box-shadow":
+                          "0px 0px 10px 5px rgba(111, 79, 28, 0.737)",
+                      }
+                    : {}
+                }
+              >
                 wolontariat
               </NavLink>
             </li>
             <li>
-              <NavLink className="link" to="/quotes" style={currentPath.startsWith("/quotes") ? {"box-shadow": "0px 0px 10px 5px rgba(111, 79, 28, 0.737)"} : {}}>
+              <NavLink
+                className="link"
+                to="/quotes"
+                style={
+                  currentPath.startsWith("/quotes")
+                    ? {
+                        "box-shadow":
+                          "0px 0px 10px 5px rgba(111, 79, 28, 0.737)",
+                      }
+                    : {}
+                }
+              >
                 ulubione cytaty
               </NavLink>
             </li>
-            
           </ul>
         </nav>
       </section>
-      
     </>
   );
 }
